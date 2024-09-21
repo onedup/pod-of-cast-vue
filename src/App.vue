f134e2a2534e20aeb75959460f75a2810ff82835
onedup <nikulichevmikhail@gmail.com>
2024-09-21 17:32:40 +0300

    feat: add fonts

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,6 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
 
@@ -22,6 +20,7 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <style scoped>
 header {
+  font-family: var(--font-family);
   line-height: 1.5;
   max-height: 100vh;
 }
--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
-import './assets/main.css'
+import 'normalize.css'
+import './assets/styles/main.scss'
 
 import { createApp } from 'vue'
 import App from './App.vue'
