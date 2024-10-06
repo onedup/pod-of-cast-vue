9bb8e6f60ea6ab156c5784e4324624b66276466e
onedup <nikulichevmikhail@gmail.com>
2024-10-06 22:05:46 +0300

    reafactor: rename component

---
+++ src/components/base/BaseDisplayTitle.vue
@@ -0,0 +1,23 @@
+<template>
+  <h2 class="title">
+    <slot></slot>
+    <!-- Основной текст заголовка -->
+    <span class="title--highlight"><slot name="title--highlight"></slot></span>
+    <!-- Текст с другим цветом -->
+  </h2>
+</template>
+
+<style scoped lang="scss">
+.title {
+  font-weight: 700;
+  font-size: 6rem;
+  line-height: 1;
+  letter-spacing: -0.04em;
+  text-align: center;
+  color: #000;
+
+  &--highlight {
+    color: var(--color-vermillion);
+  }
+}
+</style>
