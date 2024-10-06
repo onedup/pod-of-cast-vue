5a3ecd26a9ad36bb184f4029ea8a91a144c68de3
onedup <nikulichevmikhail@gmail.com>
2024-10-06 22:07:35 +0300

    feat: create component for text

---
+++ src/components/base/BaseText.vue
@@ -0,0 +1,37 @@
+<script setup>
+defineProps({
+  tagName: String,
+  size: String,
+  bold: Boolean
+})
+</script>
+
+<template>
+  <tagName :class="[`text`, `text--${props.size}`, bold ? `text--bold` : '']">
+    <slot></slot>
+  </tagName>
+</template>
+
+<style scoped lang="scss">
+.text {
+  font-weight: 500;
+  line-height: 1.6;
+  color: var(--color-black);
+
+  &--l {
+    font-size: 1rem;
+  }
+
+  &--m {
+    font-size: 0.875rem;
+  }
+
+  &--s {
+    font-size: 0.75rem;
+  }
+
+  &--bold {
+    font-weight: 700;
+  }
+}
+</style>
