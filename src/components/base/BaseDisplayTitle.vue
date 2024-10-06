8099c90e462e3f51d4d1dda25dbbceced38acfeb
onedup <nikulichevmikhail@gmail.com>
2024-10-06 23:56:47 +0300

    feat: add center props

--- src/components/base/BaseDisplayTitle.vue
+++ src/components/base/BaseDisplayTitle.vue
@@ -1,20 +1,27 @@
+<script setup>
+defineProps({
+  center: Boolean
+})
+</script>
+
 <template>
-  <h2 class="title">
+  <h2 :class="[`title`, center ? `centered` : '']">
     <slot></slot>
     <!-- Основной текст заголовка -->
-    <span class="title--highlight"><slot name="title--highlight"></slot></span>
+    <span class="title--highlight"><slot name="highlight"></slot></span>
     <!-- Текст с другим цветом -->
   </h2>
 </template>
 
 <style scoped lang="scss">
 .title {
+  margin: 0;
   font-weight: 700;
   font-size: 6rem;
   line-height: 1;
   letter-spacing: -0.04em;
-  text-align: center;
   color: #000;
+  max-width: 33.75rem;
 
   &--highlight {
     color: var(--color-vermillion);
