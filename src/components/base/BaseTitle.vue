9132dbbddc49415b50a98b222f110fa75ea32828
onedup <nikulichevmikhail@gmail.com>
2024-10-18 00:47:24 +0300

    feat: create BastTitle component for titles

---
+++ src/components/base/BaseTitle.vue
@@ -0,0 +1,69 @@
+<script setup>
+defineProps({
+  tagName: String,
+  size: String,
+  lineHeight: String,
+  bold: Boolean
+})
+</script>
+
+<template>
+  <tagName
+    :class="[
+      `title`,
+      `title--${size}`,
+      lineHeight ? `title--lh${lineHeight}` : '',
+      bold ? `title--bold` : ''
+    ]"
+  >
+    <slot></slot>
+  </tagName>
+</template>
+
+<style scoped lang="scss">
+.title {
+  font-weight: 500;
+  color: var(--color-black);
+
+  &--h1 {
+    font-weight: 700;
+    font-size: 3.6875rem;
+    letter-spacing: -0.02em;
+  }
+
+  &--h2 {
+    font-weight: 700;
+    font-size: 2.25rem;
+    letter-spacing: -0.02em;
+  }
+
+  &--h3 {
+    font-weight: 500;
+    font-size: 1.375rem;
+  }
+
+  &--h3.title--bold {
+    letter-spacing: -0.02em;
+  }
+
+  &--lh100 {
+    line-height: 1;
+  }
+
+  &--lh120 {
+    line-height: 1.2;
+  }
+
+  &--lh140 {
+    line-height: 1.4;
+  }
+
+  &--lh160 {
+    line-height: 1.6;
+  }
+
+  &--bold {
+    font-weight: 700;
+  }
+}
+</style>
